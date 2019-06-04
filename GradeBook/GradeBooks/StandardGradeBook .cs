86db49ffa218a17fe6e8d2136147dfcd9386af53
richfrankgithub <40171719+richfrankgithub@users.noreply.github.com>
2019-06-04 16:15:16 +0100

    Completed the module
    Completed the module

---
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    Public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
